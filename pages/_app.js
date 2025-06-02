f8476f4aff750fa1b440e9315f6b099b3057ee6b
Sergii <96418850+ImMrSage@users.noreply.github.com>
2025-06-02 23:04:21 +0200

    add previous and next page buttons on the homepage

--- pages/_app.js
+++ pages/_app.js
@@ -39,6 +39,7 @@ export default function App({ Component, pageProps }) {
         movies={movies}
         favorites={favorites}
         setFavorites={setFavorites}
+        page={page}
       />
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,22 @@
 import MoviesList from "@/components/MoviesList";
+import { useRouter } from "next/router";
 
-export default function HomePage({ movies, favorites, setFavorites }) {
+export default function HomePage({ movies, favorites, setFavorites, page }) {
+  const router = useRouter();
   return (
-    <MoviesList
-      movies={movies}
-      favorites={favorites}
-      setFavorites={setFavorites}
-    />
+    <>
+      <MoviesList
+        movies={movies}
+        favorites={favorites}
+        setFavorites={setFavorites}
+      />
+      <button
+        onClick={() => router.push(`/?page=${page - 1}`)}
+        disabled={page === 1}
+      >
+        ⬅️
+      </button>
+      <button onClick={() => router.push(`/?page=${page + 1}`)}>➡️</button>
+    </>
   );
 }
